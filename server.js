39ec33a9ae6f1d5c4cf5be7f8c27f61dbfa92a53
tahmina tanjil tuly <tanjiltuly@gmail.com>
2021-08-25 13:17:23 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const fileUpload = require('express-fileupload');
+const cors = require("cors")
+const app = express();
+
+app.use(cors());
+app.use(fileUpload());
+
+//UPLOAD ENDPOINT
+app.post('/upload', (req, res) => {
+    if(req.files === null) {
+        return res.status(400).json({ msg: 'no file uploaded' });
+    }
+
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if(err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}`});
+    });
+});
+
+app.listen(5000, () => console.log('Server Started...'));
